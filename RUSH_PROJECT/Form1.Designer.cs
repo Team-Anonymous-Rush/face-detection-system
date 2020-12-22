b29f2409ea9c25be74e106798e0148f47f2dc587
Shavindu Senadheera <shavinsenadeera@gmail.com>
2020-12-23 02:47:49 +0530

    Initial commit

---
+++ RUSH_PROJECT/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RUSH_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        { 
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStartCam = new System.Windows.Forms.Button();
+            this.cbDevice = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnSERVER = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartCam
+            // 
+            this.btnStartCam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartCam.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnStartCam.FlatAppearance.BorderSize = 0;
+            this.btnStartCam.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnStartCam.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnStartCam.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartCam.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartCam.ForeColor = System.Drawing.Color.Red;
+            this.btnStartCam.Location = new System.Drawing.Point(21, 79);
+            this.btnStartCam.Name = "btnStartCam";
+            this.btnStartCam.Size = new System.Drawing.Size(1216, 45);
+            this.btnStartCam.TabIndex = 0;
+            this.btnStartCam.Text = "START THE DETECTION";
+            this.btnStartCam.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnStartCam.UseVisualStyleBackColor = false;
+            this.btnStartCam.Click += new System.EventHandler(this.BtnStartCam_Click);
+            // 
+            // cbDevice
+            // 
+            this.cbDevice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbDevice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.cbDevice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbDevice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDevice.ForeColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.cbDevice.FormattingEnabled = true;
+            this.cbDevice.Location = new System.Drawing.Point(21, 29);
+            this.cbDevice.Name = "cbDevice";
+            this.cbDevice.Size = new System.Drawing.Size(1216, 33);
+            this.cbDevice.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnFile, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSERVER, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1243, 146);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(118, 237);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // btnFile
+            // 
+            this.btnFile.BackgroundImage = global::RUSH_PROJECT.Properties.Resources.icons8_documents_80;
+            this.btnFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFile.Location = new System.Drawing.Point(3, 121);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(112, 113);
+            this.btnFile.TabIndex = 9;
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.BtnFile_Click);
+            // 
+            // btnSERVER
+            // 
+            this.btnSERVER.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSERVER.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnSERVER.BackgroundImage = global::RUSH_PROJECT.Properties.Resources.icons8_happy_cloud_80;
+            this.btnSERVER.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSERVER.FlatAppearance.BorderSize = 0;
+            this.btnSERVER.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnSERVER.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnSERVER.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSERVER.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSERVER.Location = new System.Drawing.Point(3, 3);
+            this.btnSERVER.Name = "btnSERVER";
+            this.btnSERVER.Size = new System.Drawing.Size(112, 112);
+            this.btnSERVER.TabIndex = 8;
+            this.btnSERVER.UseVisualStyleBackColor = false;
+            this.btnSERVER.Click += new System.EventHandler(this.BtnSERVER_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::RUSH_PROJECT.Properties.Resources.icons8_policeman_female_80;
+            this.pictureBox1.Location = new System.Drawing.Point(1246, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(865, 130);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(21, 10);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 6;
+            this.pb2.TabStop = false;
+            this.pb2.Visible = false;
+            // 
+            // pb
+            // 
+            this.pb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb.BackColor = System.Drawing.Color.White;
+            this.pb.Location = new System.Drawing.Point(21, 146);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(1216, 678);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb.TabIndex = 2;
+            this.pb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1373, 836);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.cbDevice);
+            this.Controls.Add(this.btnStartCam);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Trafiic Violation Detection System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartCam;
+        private System.Windows.Forms.ComboBox cbDevice;
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSERVER;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnFile;
+    }
+}
+
---
+++ RUSH_PROJECT/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace RUSH_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=INDIKA;Initial Catalog=RUSH_PROJECT;Integrated Security=True");
+        Byte[] b = null;
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        string path;
+        static readonly CascadeClassifier casecadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        DateTime now = DateTime.Now;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+            {
+                cbDevice.Items.Add(device.Name);
+            }
+            cbDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+        private void BtnStartCam_Click(object sender, EventArgs e)
+        {
+            Detection();
+        }
+
+        private void Detection()
+        {
+            device = new VideoCaptureDevice(filter[cbDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImg = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = casecadeClassifier.DetectMultiScale(grayImg, 1.2, 1);
+            foreach(Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphic = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Crimson, 4))
+                    {
+                        graphic.DrawRectangle(pen, rectangle);
+                        ScreenShot();
+                    }
+                }
+            }
+            pb.Image = bitmap;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(device.IsRunning)
+            {
+                device.Stop();
+            }
+        }
+
+        private void ScreenShot()
+        {
+            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(bm);
+            g.CopyFromScreen(0,0,0,0,bm.Size);
+            pb2.Image = bm;
+            InsertInfo();
+
+        }  
+
+        private void Save()
+        {
+            var myUniqueFileName = $@"{DateTime.Now.Ticks}.txt";
+            pb2.Image.Save("C:\\xampp\\htdocs\\RUSH_DATA\\'"+ myUniqueFileName + "'.jpg");
+            path = "C:\\xampp\\htdocs\\RUSH_DATA\\'" + myUniqueFileName + "'.jpg";
+        }
+
+        private void Convereter()
+        {
+            FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(stream);
+            b = br.ReadBytes((int)stream.Length);
+        }
+
+        private void InsertInfo()
+        {
+            try
+            {
+                Save();
+                Convereter();
+                conn.Open();
+
+                string query = "INSERT INTO table_SS(SCREENS) values(@images)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.Add(new SqlParameter("@images", b));
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.ToString(),"This is an error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void BtnSERVER_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://localhost/RUSH_DATA/");
+        }
+
+        private void BtnFile_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\\xampp\\htdocs\\RUSH_DATA");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
